8289374ea74069c75e65b053ffc3477f30422787
akemilol <valeriaconcedsantos@gmail.com>
2025-09-02 11:21:35 -0300

    chore(nuget): instalar EF Infrastructure e Design na Api

---
+++ src/EasyMoto.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EasyMoto.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace EasyMoto.Infrastructure.Persistence;
+
+public sealed class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Cliente> Clientes => Set<Cliente>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}

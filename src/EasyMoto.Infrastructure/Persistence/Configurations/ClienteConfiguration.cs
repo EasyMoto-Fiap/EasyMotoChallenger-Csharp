a346a4fe2b1e2620b05915441f7fb65f9ca0dc9d
akemilol <valeriaconcedsantos@gmail.com>
2025-09-02 11:28:01 -0300

    feat(infrastructure): mapear Cliente via Fluent API

---
+++ src/EasyMoto.Infrastructure/Persistence/Configurations/ClienteConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using EasyMoto.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasyMoto.Infrastructure.Persistence.Configurations;
+
+public sealed class ClienteConfiguration : IEntityTypeConfiguration<Cliente>
+{
+    public void Configure(EntityTypeBuilder<Cliente> b)
+    {
+        b.ToTable("CLIENTE");
+        b.HasKey(x => x.IdCliente);
+        b.Property(x => x.IdCliente).HasColumnName("ID_CLIENTE").ValueGeneratedOnAdd();
+        b.Property(x => x.NomeCliente).HasColumnName("NOME_CLIENTE").IsRequired();
+        b.Property(x => x.CpfCliente).HasColumnName("CPF_CLIENTE").IsRequired();
+        b.Property(x => x.TelefoneCliente).HasColumnName("TELEFONE_CLIENTE");
+        b.Property(x => x.EmailCliente).HasColumnName("EMAIL_CLIENTE");
+    }
+}

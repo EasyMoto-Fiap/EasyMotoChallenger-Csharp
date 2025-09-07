352244ed27a94ea5fbc179a99ad7a4e0e16213c1
akemilol <valeriaconcedsantos@gmail.com>
2025-09-07 01:15:31 -0300

    feat(domain): adicionar interface ILocacaoRepository com checagem de sobreposição

---
+++ src/EasyMoto.Domain/Repositories/ILocacaoRepository.cs
@@ -0,0 +1,14 @@
+using EasyMoto.Domain.Entities;
+
+namespace EasyMoto.Domain.Repositories;
+
+public interface ILocacaoRepository
+{
+    Task<Locacao?> GetByIdAsync(int id, CancellationToken ct);
+    Task<List<Locacao>> GetAllAsync(CancellationToken ct);
+    Task AddAsync(Locacao locacao, CancellationToken ct);
+    Task UpdateAsync(Locacao locacao, CancellationToken ct);
+    Task DeleteAsync(int id, CancellationToken ct);
+    Task<bool> ExisteSobreposicaoAsync(int motoId, DateTime inicio, DateTime fim, CancellationToken ct);
+    Task SaveChangesAsync(CancellationToken ct);
+}

efe70c0749c48e071b465d53c74dee46d009f7dd
akemilol <valeriaconcedsantos@gmail.com>
2025-09-07 03:53:05 -0300

    chore(repo): remover pastas legadas; manter estrutura EasyMoto.

--- src/Application/DTOs/Response/EmpresaResponseDto.cs
+++
@@ -1,9 +0,0 @@
-namespace EasyMoto.src.Application.DTOs.Response
-{
-    public class EmpresaResponseDto
-    {
-        public int IdEmpresa { get; set; }
-        public string NomeEmpresa { get; set; }
-        public string Cnpj { get; set; }
-    }
-}

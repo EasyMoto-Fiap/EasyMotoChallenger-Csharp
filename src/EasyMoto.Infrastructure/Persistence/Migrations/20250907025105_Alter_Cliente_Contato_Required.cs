16b3261a509980067bfab13388ce6b08cd8d2752
akemilol <valeriaconcedsantos@gmail.com>
2025-09-06 23:51:59 -0300

    chore(migrations): tornar TELEFONE_CLIENTE e EMAIL_CLIENTE NOT NULL em CLIENTE

---
+++ src/EasyMoto.Infrastructure/Persistence/Migrations/20250907025105_Alter_Cliente_Contato_Required.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyMoto.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Alter_Cliente_Contato_Required : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CPF_CLIENTE",
+                table: "CLIENTE",
+                type: "NVARCHAR2(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(2000)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CLIENTE_CPF_CLIENTE",
+                table: "CLIENTE",
+                column: "CPF_CLIENTE",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CLIENTE_CPF_CLIENTE",
+                table: "CLIENTE");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CPF_CLIENTE",
+                table: "CLIENTE",
+                type: "NVARCHAR2(2000)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(450)");
+        }
+    }
+}

8209a34794c0cc8fbbe4eaa5176e60e9b8ea65d0
akemilol <valeriaconcedsantos@gmail.com>
2025-09-06 21:30:56 -0300

    feat(domain): adicionar interface IClienteRepository

---
+++ src/EasyMoto.Domain/Repositories/IClienteRepository.cs
@@ -0,0 +1,13 @@
+﻿using EasyMoto.Domain.Entities;
+
+namespace EasyMoto.Domain.Repositories;
+
+public interface IClienteRepository
+{
+    Task<Cliente?> GetByIdAsync(int id, CancellationToken ct);
+    Task AddAsync(Cliente cliente, CancellationToken ct);
+    Task UpdateAsync(Cliente cliente, CancellationToken ct);
+    Task DeleteAsync(int id, CancellationToken ct);
+    Task<bool> ExistsByCpfAsync(string cpf, CancellationToken ct);
+    Task SaveChangesAsync(CancellationToken ct);
+}
---
+++ src/EasyMoto.Infrastructure/Persistence/Migrations/20250907002447_Init_Cliente_Oracle.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyMoto.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init_Cliente_Oracle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CLIENTE",
+                columns: table => new
+                {
+                    ID_CLIENTE = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    NOME_CLIENTE = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    CPF_CLIENTE = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    TELEFONE_CLIENTE = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    EMAIL_CLIENTE = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CLIENTE", x => x.ID_CLIENTE);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CLIENTE");
+        }
+    }
+}

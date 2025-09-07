431c8266d8d388c3df44699a403caa1bb199470f
akemilol <valeriaconcedsantos@gmail.com>
2025-09-07 00:38:23 -0300

    chore(migrations): criar tabela MOTO e índice único de PLACA

---
+++ src/EasyMoto.Infrastructure/Persistence/Migrations/20250907033732_Add_Moto.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyMoto.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Moto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MOTO",
+                columns: table => new
+                {
+                    ID_MOTO = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    MODELO = table.Column<string>(type: "NVARCHAR2(120)", maxLength: 120, nullable: false),
+                    PLACA = table.Column<string>(type: "NVARCHAR2(10)", maxLength: 10, nullable: false),
+                    ANO = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    STATUS = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MOTO", x => x.ID_MOTO);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MOTO_PLACA",
+                table: "MOTO",
+                column: "PLACA",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MOTO");
+        }
+    }
+}

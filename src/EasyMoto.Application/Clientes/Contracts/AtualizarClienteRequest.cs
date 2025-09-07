b184afbd8d2b4db6e806e896727832ca903c0548
akemilol <valeriaconcedsantos@gmail.com>
2025-09-06 23:42:48 -0300

    feat(application): adicionar DTO AtualizarClienteRequest

---
+++ src/EasyMoto.Application/Clientes/Contracts/AtualizarClienteRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyMoto.Application.Clientes.Contracts;
+
+public sealed class AtualizarClienteRequest
+{
+    public string NomeCliente { get; init; } = "";
+    public string CpfCliente { get; init; } = "";
+    public string? TelefoneCliente { get; init; }
+    public string? EmailCliente { get; init; }
+}

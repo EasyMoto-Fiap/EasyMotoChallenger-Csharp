da11de554272d6e83e8b15b2ebed1298e0adfbe7
akemilol <valeriaconcedsantos@gmail.com>
2025-09-07 00:10:06 -0300

    feat(domain): adicionar base ValueObject

---
+++ src/EasyMoto.Domain/Abstractions/ValueObject.cs
@@ -0,0 +1,21 @@
+﻿namespace EasyMoto.Domain.Abstractions;
+
+public abstract class ValueObject
+{
+    protected abstract IEnumerable<object?> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not ValueObject other) return false;
+        using var a = GetEqualityComponents().GetEnumerator();
+        using var b = other.GetEqualityComponents().GetEnumerator();
+        while (a.MoveNext() && b.MoveNext())
+        {
+            if (a.Current is null ^ b.Current is null) return false;
+            if (a.Current is not null && !a.Current.Equals(b.Current)) return false;
+        }
+        return !a.MoveNext() && !b.MoveNext();
+    }
+
+    public override int GetHashCode() => GetEqualityComponents().Aggregate(1, HashCode.Combine);
+}

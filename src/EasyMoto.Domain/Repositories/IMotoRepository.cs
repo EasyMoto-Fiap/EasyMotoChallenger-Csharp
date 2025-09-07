5ac83e0327214cff594c79ab93bb2cf768ad73eb
akemilol <valeriaconcedsantos@gmail.com>
2025-09-07 00:24:20 -0300

    feat(domain): adicionar interface IMotoRepository

---
+++ src/EasyMoto.Domain/Repositories/IMotoRepository.cs
@@ -0,0 +1,14 @@
+﻿using EasyMoto.Domain.Entities;
+
+namespace EasyMoto.Domain.Repositories;
+
+public interface IMotoRepository
+{
+    Task<Moto?> GetByIdAsync(int id, CancellationToken ct);
+    Task<List<Moto>> GetAllAsync(CancellationToken ct);
+    Task AddAsync(Moto moto, CancellationToken ct);
+    Task UpdateAsync(Moto moto, CancellationToken ct);
+    Task DeleteAsync(int id, CancellationToken ct);
+    Task<bool> ExistsByPlacaAsync(string placa, CancellationToken ct);
+    Task SaveChangesAsync(CancellationToken ct);
+}

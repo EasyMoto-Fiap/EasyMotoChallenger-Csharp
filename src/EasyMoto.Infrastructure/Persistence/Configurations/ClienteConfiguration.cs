da1015fc8bc00395464429376b3b3f342d37e1c8
akemilol <valeriaconcedsantos@gmail.com>
2025-09-06 23:50:38 -0300

    refactor(infrastructure): tornar TELEFONE_CLIENTE e EMAIL_CLIENTE obrigatórios (NOT NULL) e índice único de CPF

--- src/EasyMoto.Infrastructure/Persistence/Configurations/ClienteConfiguration.cs
+++ src/EasyMoto.Infrastructure/Persistence/Configurations/ClienteConfiguration.cs
@@ -10,10 +10,27 @@ public sealed class ClienteConfiguration : IEntityTypeConfiguration<Cliente>
     {
         b.ToTable("CLIENTE");
         b.HasKey(x => x.IdCliente);
-        b.Property(x => x.IdCliente).HasColumnName("ID_CLIENTE").ValueGeneratedOnAdd();
-        b.Property(x => x.NomeCliente).HasColumnName("NOME_CLIENTE").IsRequired();
-        b.Property(x => x.CpfCliente).HasColumnName("CPF_CLIENTE").IsRequired();
-        b.Property(x => x.TelefoneCliente).HasColumnName("TELEFONE_CLIENTE");
-        b.Property(x => x.EmailCliente).HasColumnName("EMAIL_CLIENTE");
+
+        b.Property(x => x.IdCliente)
+            .HasColumnName("ID_CLIENTE")
+            .ValueGeneratedOnAdd();
+
+        b.Property(x => x.NomeCliente)
+            .HasColumnName("NOME_CLIENTE")
+            .IsRequired();
+
+        b.Property(x => x.CpfCliente)
+            .HasColumnName("CPF_CLIENTE")
+            .IsRequired();
+
+        b.Property(x => x.TelefoneCliente)
+            .HasColumnName("TELEFONE_CLIENTE")
+            .IsRequired();
+
+        b.Property(x => x.EmailCliente)
+            .HasColumnName("EMAIL_CLIENTE")
+            .IsRequired();
+
+        b.HasIndex(x => x.CpfCliente).IsUnique();
     }
 }

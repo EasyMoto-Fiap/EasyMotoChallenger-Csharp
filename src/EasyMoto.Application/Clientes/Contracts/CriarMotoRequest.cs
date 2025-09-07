5a92156c7b7d73c36fd3aa2deb85a6dbe949f35f
akemilol <valeriaconcedsantos@gmail.com>
2025-09-07 00:44:23 -0300

    feat(application): adicionar DTOs de Moto (CriarMotoRequest, MotoResponse)

---
+++ src/EasyMoto.Application/Clientes/Contracts/CriarMotoRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyMoto.Application.Motos.Contracts;
+
+public sealed class CriarMotoRequest
+{
+    public string Modelo { get; init; } = "";
+    public string Placa { get; init; } = "";
+    public int Ano { get; init; }
+}
---
+++ src/EasyMoto.Application/Clientes/Contracts/MotoResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyMoto.Application.Motos.Contracts;
+
+public sealed class MotoResponse
+{
+    public int IdMoto { get; init; }
+    public string Modelo { get; init; } = "";
+    public string Placa { get; init; } = "";
+    public int Ano { get; init; }
+    public string Status { get; init; } = "";
+}

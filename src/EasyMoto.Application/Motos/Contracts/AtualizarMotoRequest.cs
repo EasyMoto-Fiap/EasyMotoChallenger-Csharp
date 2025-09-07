2d4633a7a95d5e4021a54e4199681d0fff8cbbe2
akemilol <valeriaconcedsantos@gmail.com>
2025-09-07 01:00:56 -0300

    feat(application): adicionar DTO AtualizarMotoRequest para Moto

---
+++ src/EasyMoto.Application/Motos/Contracts/AtualizarMotoRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyMoto.Application.Motos.Contracts;
+
+public sealed class AtualizarMotoRequest
+{
+    public string Modelo { get; init; } = "";
+    public string Placa { get; init; } = "";
+    public int Ano { get; init; }
+}
